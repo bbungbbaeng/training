5f75a48e5eecdc5925109ca1fb8ace6007e68507
KIM YOUMIN <122423278+bbungbbaeng@users.noreply.github.com>
2023-12-18 21:03:18 +0900

    Greedy

--- BOJ/1541.py
+++ BOJ/1541.py
@@ -1,40 +1,11 @@
-exp = input()
-array = []
-cnt = 0
+a = input().split('-')
+res = 0
 
-for i in range(len(exp)):
-  if len(exp) == 1:
-    array.append(exp[i])
-  else:
-    if 0 < i < (len(exp) - 1):
-      if (exp[i] == '+') or (exp[i] == '-'):
-        array.append(exp[i - cnt:i])
-        array.append(exp[i])
-        cnt = 0
-      else:
-        cnt += 1
-        continue
-    elif i == 0:
-      cnt += 1
-    else:
-      array.append(exp[i - cnt:])
+for i in a[0].split('+'):
+  res += int(i)
 
-flag = False
-ans = 0
+for i in a[1:]:
+  for j in i.split('+'):
+    res -= int(j)
 
-for i in range(len(array)):
-  if (array[i] == '-'):
-    flag = True
-
-  if flag:
-    if (array[i] == '+') or (array[i] == '-'):
-      continue
-    else:
-      ans -= int(array[i])
-  else:
-    if (array[i] == '+'):
-      continue
-    else:
-      ans += int(array[i])
-
-print(ans)
+print(res)
