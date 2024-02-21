39c32ce3d6ac950bf290f516a9cb467a58cfb521
KIM YOUMIN <122423278+bbungbbaeng@users.noreply.github.com>
2024-02-21 09:39:19 +0900

    Implementation

---
+++ BOJ/14582.py
@@ -0,0 +1,15 @@
+geminis = list(map(int, input().strip().split()))
+startlink = list(map(int, input().strip().split()))
+
+g_sum, s_sum, flag = 0, 0, False
+
+for i in range(9):
+    g_sum += geminis[i]
+    if g_sum > s_sum:
+        flag = True
+    s_sum += startlink[i]
+
+if g_sum < s_sum and flag:
+    print('Yes')
+else:
+    print('No') 
